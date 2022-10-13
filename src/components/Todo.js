2a9eb5648f6e3071ee404b5216fea16716d31e4b
boazKAD <95119645+boazKAD@users.noreply.github.com>
2022-10-13 03:15:10 -0700

    Add files via upload

---
+++ src/components/Todo.js
@@ -0,0 +1,27 @@
+import Backdrop from './Backdrop';
+import Modal from './Modal';
+import { useState} from 'react'
+function Todo (props){
+    const [modalIsOpen,setmodalIsOpen]=useState(false);
+    function deleteHendler() {
+        setmodalIsOpen(true);
+    }
+    function closeModalHandler(){
+        setmodalIsOpen(false);
+    }
+    
+    return(
+        <div className="card">
+           <h2>{props.text}</h2>
+           <div className="actions">
+              <button className="btn" onClick={deleteHendler}>Delete</button>
+           </div>
+           {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
+           {modalIsOpen && <Backdrop onCancel={closeModalHandler}/>}
+      </div>
+    )
+     
+    
+}
+export default Todo;
+
